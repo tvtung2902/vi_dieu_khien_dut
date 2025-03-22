329f43329bab7d9863dc8bbd0b07fd75d05263ad
Tran Van Tung <123tranvantung456@gmail.com>
2025-03-23 00:02:24 +0700

    update

--- Phần 1 - Cơ Bản/ngat/test_1/test_1.ino
+++ Phần 1 - Cơ Bản/ngat/test_1/test_1.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+#define LED 13
 
+void setup() {
+    Serial.begin(9600);
+    pinMode(LED, OUTPUT);
+    pinMode(2, INPUT);
+    pinMode(3, INPUT);
+    attachInterrupt(digitalPinToInterrupt(2), displayMicros, RISING);
+    attachInterrupt(digitalPinToInterrupt(3), displayMillis, RISING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    int btnPin_2 = digitalRead(2);
+    int btnPin_3 = digitalRead(3);
+    Serial.println(btnPin_2);
+    Serial.println(btnPin_3);
 
+    digitalWrite(LED, HIGH);
+    delay(500);
+    digitalWrite(LED, LOW);
+    delay(500);
+}
+
+void displayMicros() {
+    Serial.print("micros() = ");
+    Serial.println(micros());
+}
+
+void displayMillis() {
+    Serial.print("millis() = ");
+    Serial.println(millis());
 }
---
+++ Phần 1 - Cơ Bản/ngat/test_2/test_2.ino
@@ -0,0 +1,30 @@
+#define LED 13
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(LED, OUTPUT);
+    pinMode(2, INPUT);
+    pinMode(3, INPUT);
+    attachInterrupt(digitalPinToInterrupt(2), ledOn, RISING);
+    attachInterrupt(digitalPinToInterrupt(3), ledOff, RISING);
+}
+
+void loop() {
+    int btnPin_2 = digitalRead(2);
+    int btnPin_3 = digitalRead(3);
+    Serial.println(btnPin_2);
+    // Serial.println(btnPin_3);
+
+    // digitalWrite(LED, HIGH);
+    // delay(500);
+    // digitalWrite(LED, LOW);
+    delay(500);
+}
+
+void ledOn() {
+    digitalWrite(LED, HIGH);
+}
+
+void ledOff() {
+  digitalWrite(LED, LOW);
+}
